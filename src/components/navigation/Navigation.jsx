0266cb5689903b6451e897437d339e0e59687032
Yakob Eliyo <Yakob.Eliyo@yh.nackademin.se>
2024-02-18 11:52:24 +0100

    add changes to route

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,4 +1,3 @@
-
 import { NavLink } from "react-router-dom";
 
 //Komponent som hanterar navigeringen i applikationen
--- src/components/partials/Routing.jsx
+++ src/components/partials/Routing.jsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import ProductDetail from "../views/ProductDetail";
 import Home from "../views/Home";
-
+import ProductInsert from '../views/ProductInsert';
+import ProductEdit from '../views/ProductEdit';
 
 const Routing = () => {
     return(
